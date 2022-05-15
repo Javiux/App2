778df40ee228e1f807bbd4396fc85cda03e90087
ImaSalas <92271130+ImaSalas@users.noreply.github.com>
2022-05-15 17:48:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -58,13 +58,6 @@
 	?>	
       </table>
 	</div>
-	<script>
-$(document).ready(function() {
-      var refreshId =  setInterval( function(){
-    $('#actualiza').load('refresh.php');//actualizas el div
-   }, 1000 );
-});
 
-</script>
 </body>
 </html>
