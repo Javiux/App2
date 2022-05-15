b4b55cdb3c378be6de10fb77a8179b5af2aa0620
ImaSalas <92271130+ImaSalas@users.noreply.github.com>
2022-05-15 17:43:10 -0500

    Update refresh.php

--- refresh.php
+++ refresh.php
@@ -1,2 +1,20 @@
 <div id="actualiza">
+  <table border="1">
+	  <tr>
+		<td>Nombre</td>
+		<td>Apellido</td>
+	  </tr>
+	 <?php
+		$sql="SELECT * FROM usuario";
+		$result=mysqli_query($conexion,$sql);
+	        while($mostrar=mysqli_fetch_array($result)){	
+	 ?>
+    	  <tr>
+		<td><?php echo $mostrar['Nombre']?></td>
+		<td><?php echo $mostrar['Apellido']?></td>
+	  </tr>
+	<?php
+	}
+	?>	
+      </table>
 </div>
