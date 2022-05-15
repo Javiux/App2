803734eac2f75dc780628ec9a229a07671a81df0
ImaSalas <92271130+ImaSalas@users.noreply.github.com>
2022-05-15 15:02:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+
+	$conexion=mysqli_connect('20.25.51.254','server','123456789','newAzure');
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -30,5 +36,26 @@
 		</div>
 	</div>
 	<div class="msjRespuesta"></div>
+<br>
+<form action="php/proceso.php" method="POST" class="AjaxForm">
+	<table border="1">
+	  <tr>
+		<td>Nombre</td>
+		<td>Apellido</td>
+	  </tr>
+	 <?php
+		$sql="SELECT * FROM usuario";
+		$result=mysqli_query($conexion,$sql);
+	        while($mostrar=mysqli_fetch_array($result)){	
+	 ?>
+    	  <tr>
+		<td>php echo $mostrar['Nombre']</td>
+		<td>php echo $mostrar['Apellido']</td>
+	  </tr>
+	<?php
+	}
+	?<	
+      </table>
+</form>
 </body>
 </html>
