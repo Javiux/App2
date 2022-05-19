b1a83a73664c8bd89d3b123cdef692db98b49fbd
Javiux <92271705+Javiux@users.noreply.github.com>
2022-05-18 23:21:13 -0500

    a

--- php/proceso.php
+++ php/proceso.php
@@ -1,8 +1,8 @@
 <?php
 	include '../library/SGBD.php';
 	$nombre=$_POST['nombre'];
-	$num_control=$_POST['numero_control'];
-	if(SGBD::Insert("alumnos", "nombre, numero_control" "'$nombre', '$num_control'")){
+	$numero_control=$_POST['numero_control'];
+	if(SGBD::Insert("alumnos", "nombre, numero_control" "'$nombre', '$numero_control'")){
 		echo "<script> alert ('Datos guardados'); </script>";
 	}else{
 		echo "<script> alert ('Error: Datos no guardados'); </script>";
